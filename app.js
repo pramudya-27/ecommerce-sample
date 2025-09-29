a22f3e7a9189e55abd005107b7ee1a89a2ff232a
pramudya-27 <DIO2431058@itpln.ac.id>
2025-09-29 22:08:10 +0700

    change PORT

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8080
 const HOST = '0.0.0.0'
 
 // Middleware untuk file statis
