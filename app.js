9bc4202232b9ae00912e5b64c58c2aaaa3e117a1
pramudya-27 <DIO2431058@itpln.ac.id>
2025-09-29 16:21:37 +0700

    main

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 8000
+const HOST = '0.0.0.0'
+
+// Middleware untuk file statis
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Home
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+// Profile
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'profile.html'))
+})
+
+// Cart
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'cart.html'))
+})
+
+// Contact
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contact.html'))
+})
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`)
+})
